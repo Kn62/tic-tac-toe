e38f984aebe50038da882c350ee611d279dd0b60
Kn62 <knlecocq@gmail.com>
2024-04-17 17:03:48 +0200

    made the div to display player info and game zone plus settings buttons

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const gameMode = document.querySelector(".first-container");
 const playerNameMode = document.querySelector(".second-container");
+const playMode = document.querySelector(".third-container");
 const player1Name = document.querySelector("#first-player");
 const player2Name = document.querySelector("#second-player");
+const gameOptions = document.querySelectorAll(".game-options");
 const buttonPvp = document.querySelector(".button-pvp");
 const buttonPve = document.querySelector(".button-pve");
-const buttonBack = document.querySelector(".back-button")
-const buttonNext = document.querySelector(".next-button")
-const playMode = document.querySelector(".third-container") 
+const buttonBack = document.querySelector(".back-button");
+const buttonNext = document.querySelector(".next-button");
+const buttonNewGame = document.querySelector(".newgame-button");
+const buttonReset = document.querySelector(".reset-button");
+
+const displayName = function (one, two ) {
+    const displayName1 = document.querySelector(".player-one-name");
+    const displayName2 = document.querySelector(".player-two-name");
+    displayName1.innerText = one;
+    displayName2.innerText = two;
+}
 
 const swapDisplayContainer = function (from, to) {
  from.style.display = "none";
@@ -20,16 +30,23 @@ buttonPvp.addEventListener("click", () => {
 buttonPve.addEventListener("click", () => {
     swapDisplayContainer(gameMode, playerNameMode);
     player2Name.value = "BOT";
+    player2Name.disabled = "true";
 })
 
 buttonBack.addEventListener("click", () => {
-    swapDisplayContainer(playerNameMode, gameMode)
+    window.location.reload();
 })
+
+buttonNewGame.addEventListener("click", () => {
+    window.location.reload();
+})
+
+// buttonReset.addEventListener("click", () => {
+
+// })
 
 buttonNext.addEventListener("click", () => {
     swapDisplayContainer(playerNameMode, playMode)
-    const player1 = player1Name.value;
-    const player2 = player2Name.value;
-    console.log(player1);
-    console.log(player2);
+    displayName(player1Name.value, player2Name.value)
+
 })
