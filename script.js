499e9a7d9b9b639863b419a96703339f8e933055
Kn62 <knlecocq@gmail.com>
2024-04-19 14:20:52 +0200

    adding a winning condition and how to react when there's a winner

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const gameMode = document.querySelector(".first-container");
 const playerNameMode = document.querySelector(".second-container");
 const playMode = document.querySelector(".third-container");
 const player1Name = document.querySelector("#first-player");
+const player1ScoreDisplay = document.querySelector(".scores-player-one");
 const player2Name = document.querySelector("#second-player");
+const player2ScoreDisplay = document.querySelector(".scores-player-two");
 const gameOptions = document.querySelectorAll(".game-options");
+const gameZone = document.querySelector(".game-zone");
 const buttonPvp = document.querySelector(".button-pvp");
 const buttonPve = document.querySelector(".button-pve");
 const buttonBack = document.querySelector(".back-button");
@@ -11,7 +14,15 @@ const buttonNext = document.querySelector(".next-button");
 const buttonNewGame = document.querySelector(".newgame-button");
 const buttonReset = document.querySelector(".reset-button");
 
+
+
 let isPVE = false; 
+let gameArray = [];
+let player1Score = 0;
+let player2Score = 0;
+let curentTurn = 1;
+player1ScoreDisplay.innerText = player1Score;
+player2ScoreDisplay.innerText = player2Score;
 
 const playerFactory = function (nickname, choice) {
     const getNickname =  nickname;
@@ -28,31 +39,37 @@ const displayName = function (one, two ) {
 
 const startNewGame = function () {
     const playerOneInfo = playerFactory(player1Name.value, "X");
-    const playerTwoInfo = playerFactory(player2Name.value, "O");
-    let curentTurn = "1"
-    let gameArray = []
+    const playerTwoInfo = playerFactory(player2Name.value, "O");    
     
     for (let i = 0; i < gameOptions.length; i++) {
         gameArray.push(`${gameOptions[i].innerText}`)
 
         gameOptions[i].addEventListener("click", () => {
             if (gameOptions[i].innerText === "") {
-                if (curentTurn === "1") {
+                if (curentTurn === 1) {
                     gameOptions[i].innerText = playerOneInfo.getChoice;
                     gameArray[i] = playerOneInfo.getChoice
-                    curentTurn = "0"
+                    curentTurn = 0
                 } else {
                     gameOptions[i].innerText = playerTwoInfo.getChoice;
                     gameArray[i] = playerTwoInfo.getChoice
-                    curentTurn = "1"
+                    curentTurn = 1
                 }
             }
             console.log(gameArray)
-            // winningCheck(gameArray)
+            winningCheck(gameArray)
         } )
     }
 
 
+}
+
+const resetGameArray = function () {
+    for ( let i = 0; i < gameOptions.length; i++) {
+        gameOptions[i].innerText = "";
+        gameOptions[i].classList = "game-options"
+        gameArray[i] = ""
+    }
 }
 
 const swapDisplayContainer = function (from, to) {
@@ -80,41 +97,65 @@ buttonNewGame.addEventListener("click", () => {
 })
 
 buttonReset.addEventListener("click", () => {
-
+    resetGameArray();
+    curentTurn = 1
+    but
 })
 
 buttonNext.addEventListener("click", () => {
-    // const playerOneInfo = playerFactory(player1Name.value, "X");
-    // const playerTwoInfo = playerFactory(player2Name.value, "O");
     startNewGame();
-
-
     swapDisplayContainer(playerNameMode, playMode);
     displayName(player1Name.value, player2Name.value);
 
 })
 
-// const winningCheck = function (array) {
-//     const winningCondition = [
-//         [0, 1, 2]
-//         [0, 4, 8]
-//         [0, 3, 6]
-//         [1, 4, 7]
-//         [2, 4, 6]
-//         [2, 5, 8]
-//         [3, 4, 5]
-//         [6, 7, 8]
-//     ]
+const winningCheck = function (array) {
+    const winningCondition = [
+        [0, 1, 2],
+        [0, 4, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 4, 6],
+        [2, 5, 8],
+        [3, 4, 5],
+        [6, 7, 8],
+    ]
+    
+    for (let i = 0; i < winningCondition.length; i++) {
+        const firstCondition = winningCondition[i][0];
+        const secondCondition = winningCondition[i][1];
+        const thirdCondition = winningCondition[i][2];
 
-//     for (let i = 0; i < winningCondition.length; i++) {
-//         const[a, b, c] = winningCondition[i];
-//         if (array[a] !== "") {
+        if (array[firstCondition] !== "" && 
+            array[firstCondition] === array[secondCondition] &&
+            array[secondCondition] === array[thirdCondition]) {
+                announceWinner(firstCondition, secondCondition, thirdCondition);
+                updateScore(array[firstCondition]);
+                lockGameZone();
+            }
+        }
+    }        
 
-//             if (array[a] === array[b] &&
-//                 array[b] === array[c]) {
-//                     console.log("win")
-//             }
-//         }
-//     }        
-// }
+const announceWinner = function(first, second, third) {
+    gameOptions[first].classList.add("winner")
+    gameOptions[second].classList.add("winner")
+    gameOptions[third].classList.add("winner")
+}
+
+const updateScore = function(choice) {
+    if (choice === "X") {
+        player1Score++;
+        player1ScoreDisplay.innerText = player1Score;
+    } else {
+        player2Score++;
+        player2ScoreDisplay.innerText = player2Score;
+    }
+}
+
+const lockGameZone = function () {
+    for (let i = 0; i < gameOptions.length ; i++) {
+        gameOptions[i].classList.add("locked")
+    }
+    buttonReset.innerText = "Play Again";
+}
 
